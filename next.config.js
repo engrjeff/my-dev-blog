a00f2b362ec386ccfdf9cac5e26d90fd15552cb3
Jeffrey Segovia <jeffrey.segovia@opsolutions.biz>
2022-10-02 13:45:33 +0800

    added lang to html tag

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
