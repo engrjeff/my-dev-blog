0ff094cda4a55909720ad4308a9d86a97ff35e04
Jeffrey Segovia <jeffrey.segovia@opsolutions.biz>
2022-11-17 15:38:39 +0800

    changed style of code

--- components/Article.tsx
+++ components/Article.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from 'react';
 
 const Article = ({ children }: { children: ReactNode }) => {
   return (
-    <article className='prose lg:prose-lg mx-auto prose-a:no-underline hover:prose-a:text-primary prose-code:text-primary prose-blockquote:font-normal prose-img:rounded-lg prose-img:m-0 prose-headings:text-left prose-img:mx-auto dark:prose-invert'>
+    <article className='prose lg:prose-lg mx-auto prose-code:bg-gray-700 prose-code:font-medium prose-code:before:hidden prose-code:after:hidden prose-code:px-1 prose-code:rounded prose-code:text-white prose-a:no-underline hover:prose-a:text-primary prose-blockquote:font-normal prose-img:rounded-lg prose-img:m-0 prose-headings:text-left prose-img:mx-auto dark:prose-invert'>
       {children}
     </article>
   );
