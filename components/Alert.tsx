e48b788ffbcc123f9efeffd6387379b3c9b90410
engrjeff <engineerjeff15@gmail.com>
2024-07-29 17:54:09 +0800

    update Alert styles

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -2,9 +2,9 @@ import { type ReactNode } from 'react';
 
 const Alert = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='not-prose bg-sky-100 p-4 my-8 text-[15px] border-l-4 border-sky-500'>
-      <p className='font-bold uppercase text-sky-500'>💡 Info</p>
-      <div className='text-gray-600 my-2 [&>p]:inline-block'>{children}</div>
+    <div className="not-prose bg-blue-600/10 p-4 my-8 text-[15px] border-l-4 border-sky-500">
+      <p className="font-bold uppercase text-sky-500">💡 Info</p>
+      <div className="text-white my-2 [&>p]:inline-block">{children}</div>
     </div>
   );
 };
