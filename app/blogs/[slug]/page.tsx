f0d1927d02baa48ebb79896244a622c7a5de6709
engrjeff <engineerjeff15@gmail.com>
2024-08-09 00:07:05 +0800

    enable blog screen generateStaticParams

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -35,17 +35,15 @@ export const generateMetadata = ({
   };
 };
 
-// export const generateStaticParams = () => {
-//   return allPosts
-//     .map((post) => ({ slug: post.slug }))
-//     .filter(
-//       (s) =>
-//         s.slug !==
-//         'file-tree-component-using-react-server-components-and-tailwindcss'
-//     );
-// };
-
-export const dynamic = 'force-dynamic';
+export const generateStaticParams = () => {
+  return allPosts
+    .map((post) => ({ slug: post.slug }))
+    .filter(
+      (s) =>
+        s.slug !==
+        'file-tree-component-using-react-server-components-and-tailwindcss'
+    );
+};
 
 function BlogScreen({ params }: { params: { slug: string } }) {
   const post = allPosts.find((post) => post.slug === params.slug);
