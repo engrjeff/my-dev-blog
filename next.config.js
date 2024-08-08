1de1394845ea15adfe1fbdfdbda83bd652c5457f
engrjeff <engineerjeff15@gmail.com>
2024-08-09 00:07:14 +0800

    add outputFileTracingIncludes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const { withContentlayer } = require('next-contentlayer');
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/file-tree-app': ['./app'],
+      '/file-tree-example': ['./components/FileTree/example'],
+    },
+  },
   swcMinify: true,
   images: {
     remotePatterns: [
